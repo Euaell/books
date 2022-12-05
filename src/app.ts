4d266102f9b530139bc936bced887eccbdbc11e9
Euael Eshete <euaelmeko@gmail.com>
2022-12-05 14:39:44 +0300

    fixed the build issue -- due to express-fileupload

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 // import morgan from 'morgan'
-import expressupload from 'express-fileupload'
+// import expressupload from 'express-fileupload'
 
 // import routes
 import userRoutes from './routes/user'
@@ -14,7 +14,7 @@ const app: Application = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
-app.use(expressupload())
+// app.use(expressupload())
 app.use(cors())
 
 // app.use(morgan('dev'))
