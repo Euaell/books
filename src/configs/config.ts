b31199641bb86c89311308273513a7cf3d21988e
Euael Eshete <euaelmeko@gmail.com>
2022-12-05 14:55:01 +0300

    fixed the issue with exposed db username and pass

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,12 +1,16 @@
-const DB_URI = process.env.MONGO_URI || "mongodb+srv://client:ciB0f2JaPkCfS0uV@cluster0.dksbqbi.mongodb.net/BookDB"
+import dotenv from 'dotenv'
+dotenv.config()
+
+const DB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/BookDB"
 
 const PORT = process.env.PORT || 5000
 const JWT_SECRET = process.env.JWT_SECRET || "secret"
 const MaxAge = 1000 * 60 * 60 * 24 // 24 hours
 const EMAIL = process.env.EMAIL || "email"
 const PASSWORD = process.env.PASSWORD || "12345678"
-const DB_USERNAME = process.env.DB_USERNAME || "client"
-const DB_PASSWORD = process.env.DB_PASSWORD || "ciB0f2JaPkCfS0uV"
+
+const DB_USERNAME = process.env.DB_USERNAME || "username"
+const DB_PASSWORD = process.env.DB_PASSWORD || "password"
 
 
 const configs = {
